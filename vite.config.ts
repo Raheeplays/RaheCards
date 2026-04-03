0603bcb0676e3105fd1c61b7e5aa9ab739468714
Raheeplays <raheeplays5@gmail.com>
2026-04-03 08:09:05 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // Yeh aapki design (CSS) ko build mein include karega
+  ],
+  base: '/RaheeCards/', // Isse CSS files ka path sahi ho jayega
+})
